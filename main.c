adbb3cd037227fbb91b201ba062452a753be43bc
Nicholas Chiapputo <chiapputo.nick@gmail.com>
2019-11-11 15:44:40 -0600

    Implemented turning and switch for forward/backward.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 #define CLOCK_HF		48000000 	// Hz
 #define CLOCK_LF		32000 		// Hz
-#define SAMPLE_FREQ		100			// Hz. ADC can't take more than 24 MHz per sample. (P6.0 and P6.1 are each 1 sample). 50 Hz per sample is enough I think
+#define SAMPLE_FREQ		1000			// Hz. ADC can't take more than 24 MHz per sample. (P6.0 and P6.1 are each 1 sample). 50 Hz per sample is enough I think
 
 #define HEARTBEAT_FREQ	2  			// unit: Hz, heart beat LED blinking frequency for debugging
 
@@ -131,51 +131,10 @@ void main(void)
 				// Restart motors
 				startMotor( LEFT_MOTOR );
 				startMotor( RIGHT_MOTOR );
-
-
-				// Turn away from wall example
-				// Save previous duty cycles
-//				int tempLeft = getMotorDutyCycle( LEFT_MOTOR );
-//				int tempRight = getMotorDutyCycle( RIGHT_MOTOR );
-//
-//				// Turn to the left by stopping the left motor and turning on the right motor
-//				setMotorDutyCycle( LEFT_MOTOR, 0 );
-//				setMotorDutyCycle( RIGHT_MOTOR, 30 );
-//
-//				// Wait a bit as it turns away from the wall.
-//				// This should be done with a timer with period set relative to the tachometer reading.
-//				int i = 0;
-//				for( i = 0; i < 1000000; i++ );
-//
-//				// Set motors back to previous state
-//				setMotorDutyCycle( LEFT_MOTOR, tempLeft );
-//				setMotorDutyCycle( RIGHT_MOTOR, tempRight );
-
 			}
 			else
 			{
-				// Stop motors while bump sensor is active
-//				pauseMotor( LEFT_MOTOR );
-//				pauseMotor( RIGHT_MOTOR );
-
-
-
-				// Move away from wall
-				backingUp = 1;
-				setMotorDirection( LEFT_MOTOR, MOTOR_BACKWARD );
-				setMotorDirection( RIGHT_MOTOR, MOTOR_BACKWARD );
-
-				// Wait just a second while it moves away from the wall
-				int i = 0;
-				for( i = 0; i < 1000000; i++ );
-
-				// Reset motor direction
-				setMotorDirection( LEFT_MOTOR, MOTOR_FORWARD );
-				setMotorDirection( RIGHT_MOTOR, MOTOR_FORWARD );
-				backingUp = 0;
-
 				// Pause motors until button is released.
-				// In practical application, a turn could be added after the motor is restarted
 				pauseMotor( LEFT_MOTOR );
 				pauseMotor( RIGHT_MOTOR );
 
@@ -214,13 +173,16 @@ void initADC14()
 
 	//Configure P6.1 as A14 and P6.0 as A15
 	GPIO_setAsPeripheralModuleFunctionInputPin( GPIO_PORT_P6, GPIO_PIN0 | GPIO_PIN1, GPIO_TERTIARY_MODULE_FUNCTION );
+    GPIO_setAsPeripheralModuleFunctionInputPin( GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN2, GPIO_TERTIARY_MODULE_FUNCTION );
 
 	// Configure non-repeating multi-sequence memory storage in registers 0 and 1
-	ADC14_configureMultiSequenceMode( ADC_MEM0, ADC_MEM1, false );
+	ADC14_configureMultiSequenceMode( ADC_MEM0, ADC_MEM3, false );
 
 	// Store P6.1 (A14) reading in MEM0 and P6.0 (A15) reading in MEM1
 	ADC14_configureConversionMemory( ADC_MEM0, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A14, false );
 	ADC14_configureConversionMemory( ADC_MEM1, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A15, false );
+    ADC14_configureConversionMemory( ADC_MEM2, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A13, false );
+    ADC14_configureConversionMemory( ADC_MEM3, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A11, false );
 
     //See TechRef Section 20.2.6 for sample timing consideration.
     ADC14_enableSampleTimer( ADC_MANUAL_ITERATION );
@@ -232,15 +194,15 @@ void initADC14()
 void initHeartBeatLED()
 {
 	GPIO_setAsOutputPin( GPIO_PORT_P2, GPIO_PIN0 | GPIO_PIN1 | GPIO_PIN2 );
-
-	Timer32_initModule( TIMER32_0_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT, TIMER32_PERIODIC_MODE );
-	Timer32_setCount( TIMER32_0_BASE, clockMCLK / HEARTBEAT_FREQ );
-	Timer32_enableInterrupt( TIMER32_0_BASE );
-	Interrupt_enableInterrupt( INT_T32_INT1 );
 }
 
 void initTimer()
 {
+    Timer32_initModule( TIMER32_0_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT, TIMER32_PERIODIC_MODE );
+    Timer32_setCount( TIMER32_0_BASE, clockMCLK / HEARTBEAT_FREQ );
+    Timer32_enableInterrupt( TIMER32_0_BASE );
+    Interrupt_enableInterrupt( INT_T32_INT1 );
+
     Timer32_initModule( TIMER32_1_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT, TIMER32_PERIODIC_MODE );
     Timer32_setCount( TIMER32_1_BASE, clockMCLK / SAMPLE_FREQ );
     Timer32_enableInterrupt( TIMER32_1_BASE );
@@ -261,7 +223,7 @@ void T32_INT2_IRQHandler()
 {
 	Timer32_clearInterruptFlag( TIMER32_1_BASE );
 
-	uint16_t dataInt[ 2 ] = { 0 };
+	uint16_t dataInt[ 4 ] = { 0 };
 	float dutyCycle, steering;
 
 	//Read from ADC.
@@ -271,7 +233,7 @@ void T32_INT2_IRQHandler()
 	ADC14_getMultiSequenceResult( dataInt );
 
 
-	//dutyCycle = 200 * ( float ) dataInt[ 0 ] / 0x3FFF - 100;	// Calculate on range of [ -100, 100 ]. <0 means backwards, >0 means forwards
+//	dutyCycle = 200 * ( float ) dataInt[ 0 ] / 0x3FFF - 100;	// Calculate on range of [ -100, 100 ]. <0 means backwards, >0 means forwards
 	dutyCycle = 100 * ( float ) dataInt[ 0 ] / 0x3FFF;
 	steering = 100 * ( float ) dataInt[ 1 ] / 0x3FFF;
 
@@ -290,11 +252,14 @@ void T32_INT2_IRQHandler()
 //	snprintf( str, 100, "P6.1 Data In = %0.3f; P4.0 Data Out = %i   \r", data, dataOut );
 
 	// Used to show the duty cycle and steering ratio using potentiometers
-	snprintf( str, 100, "Duty Cycle = %i%%; Duty Cycle Ratio (L:R): %i%%: %i:%i    \r", ( int ) dutyCycle, ( int )steering, leftDC, rightDC );
-	uart0_transmitStr( str );
+//	snprintf( str, 100, "Duty Cycle = %i%%; Duty Cycle Ratio (L:R): %i%%: %i:%i; Forward: %i     \r", ( int ) dutyCycle, ( int )steering, ( int ) dutyCycle * leftDC / 100, ( int ) dutyCycle * rightDC / 100, dataInt[ 2 ] >= 16000 ? MOTOR_FORWARD : MOTOR_BACKWARD );
+//	uart0_transmitStr( str );
 
-	setMotorDutyCycle( LEFT_MOTOR, ( int ) dutyCycle );
-	setMotorDutyCycle( RIGHT_MOTOR, ( int ) dutyCycle );
+	setMotorDutyCycle( LEFT_MOTOR, ( int ) dutyCycle * leftDC / 100 );
+	setMotorDutyCycle( RIGHT_MOTOR, ( int ) dutyCycle * rightDC / 100 );
+
+	setMotorDirection( LEFT_MOTOR, dataInt[ 2 ] >= 16000 ? MOTOR_FORWARD : MOTOR_BACKWARD );
+    setMotorDirection( RIGHT_MOTOR, dataInt[ 2 ] >= 16000 ? MOTOR_FORWARD : MOTOR_BACKWARD );
 
 	// TODO: setMotorDirection based on sign of dutyCycle when using the first equation
 	// TODO: Implement steering using leftDC and rightDC
